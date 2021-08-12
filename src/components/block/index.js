5176c94f80f1ecfec0ce565c496b9e067499693b
Tuusoo <suhao1998@yeah.net>
2021-08-13 00:57:52 +0800

    add:选中样式

--- src/components/block/index.js
+++ src/components/block/index.js
@@ -7,28 +7,28 @@ function block(props) {
   switch(props.type){
     case "todo":
       return(
-        <section className="chooseLabel">
+        <section className={props.isClick?"clicking chooseLabel":"chooseLabel"}>
           <img src={ vueimg } className="logo" alt="vue"/>
           <section className="text">待办事项</section>
         </section>
       );
     case "doing":
       return(
-        <section className="chooseLabel">
+        <section className={props.isClick?"clicking chooseLabel":"chooseLabel"}>
           <img src={ reactimg } className="logo" alt="react"/>
           <section className="text">正在处理</section>
         </section>
       )
     case "done":
       return(
-        <section className="chooseLabel">
+        <section className={props.isClick?"clicking chooseLabel":"chooseLabel"}>
           <img src={ angularimg } className="logo" alt="react"/>
           <section className="text">已办事项</section>
         </section>
       )
     default:
       return(
-        <section className="chooseLabel">
+        <section className={props.isClick?"clicking chooseLabel":"chooseLabel"}>
           <img src={ angularimg } className="logo" alt="react"/>
           <section className="text"></section>
         </section>
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,12 +1,28 @@
-//import { useState } from 'react';
+import { useState } from 'react';
 import Block from '../components/block';
 import '../style/App.less'
 
 function App() {
-  let listType = ["todo", "doing", "done"];
+  /* let listType = ["todo", "doing", "done"];
   let listTitles = [];
   listTitles = listType.map((i) => 
     <Block type={i} key={i}></Block>
+  ) */
+  const [listType, setListType] = useState([
+    {
+      title: "todo",
+      state: true,
+    },{
+      title: "doing",
+      state: false,
+    },{
+      title: "done",
+      state: false,
+    }
+  ]);
+  let listTitles = [];
+  listTitles = listType.map((i) => 
+    <Block type={i.title} isClick={i.state} key={i.title}></Block>
   )
   return (
     <section className="body">
