9a9c753c12fafa05e82c6fb389d9bdd94a8af466
Tuusoo <suhao1998@yeah.net>
2021-08-17 01:01:10 +0800

    加了列表item组件，待办增加item功能，但添加item有问题，怀疑是冒泡原因，明天再解决:(

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -1,3 +1,25 @@
-function Item() {
-  return
+import { useState } from "react"
+
+import "../../style/item.less"
+
+import trash from "../../images/trash.svg"
+import trashSelected from "../../images/trashSelected.svg"
+import unselected from "../../images/unselected.svg"
+
+function Item(props) {
+  const [isCover, setIsCover] = useState(false);
+  const [isTrashCover, setIsTrashCover] = useState(false);
+  const mouseOver = () => setIsCover(true);
+  const mouseOut = () => setIsCover(false);
+  const trashMouseOver = () => setIsTrashCover(true);
+  const trashMouseOut = () => setIsTrashCover(false);
+  return (
+    <section className="item" onMouseOver={mouseOver} onMouseOut={mouseOut}>
+      <img className="check" src={unselected} alt="check"/>
+      <section className="text">{props.text}</section>
+      <img className="delete" src={isTrashCover?trashSelected:trash} alt="delete" style={isCover?{}:{display: "none"}} onMouseOver={trashMouseOver} onMouseOut={trashMouseOut}/>
+    </section>
+  )
 }
+
+export default Item;
--- src/components/ItemList/index.js
+++ src/components/ItemList/index.js
@@ -1,22 +1,64 @@
+import { useState } from "react"
+
+import Item from "../Item";
+
+import "../../style/itemList.less"
+
+import plusGray from "../../images/plusGray.svg"
+import plusblack from "../../images/plusBlack.svg"
+
 function ItemList(props) {
-  switch(props.tab){
+  const[listArrays, setListArrays] = useState({
+    todo: [],
+    doing: [],
+    done: []
+  });
+  const[isAddCover, setIsAddCover] = useState(false);
+  const[isAddIconCover, setIsAddIconCover] = useState(false);
+  const[inputText, setInputText] = useState("");
+  
+  let itemsArray = [];
+  switch(props.tab) {
     case "todo":
-      return(
-        <section>123</section>
-      )
+      itemsArray = listArrays.todo;
+      break;
     case "doing":
-      return(
-        <section>234</section>
-      )
-    case "doing":
-      return(
-        <section>456</section>
-      )
+      itemsArray = listArrays.doing;
+      break;
+    case "done":
+      itemsArray = listArrays.done;
+      break;
     default:
-      return(
-        <section>5494689</section>
-      )
+      itemsArray = listArrays.todo;
+      break;
   }
+  let itemsTemplate = <section></section>;
+  itemsTemplate = itemsArray.map((i, index) => <Item text={i} index={index} key={index}></Item>)
+
+  const addMouseOver = () => setIsAddCover(true);
+  const addMouseOut = () => setIsAddCover(false);
+
+  const iconMouseOver = () => setIsAddIconCover(true);
+  const iconMouseOut = () => setIsAddIconCover(false);
+
+  const inputing = (e) => setInputText(e.target.value);
+
+  const addIntoList = () => {
+    setListArrays(() => {
+      let setArray = listArrays;
+      setArray.todo = [inputText,...setArray.todo];
+      return setArray;
+    });
+  }
+  return (
+    <section>
+      <section style={props.tab === "todo"?{}:{display: "none"}} className="addItem" onMouseOver={addMouseOver} onMouseOut={addMouseOut}>
+        <input onInput={inputing}></input>
+        <img src={isAddIconCover?plusblack:plusGray} style={isAddCover?{}:{display: "none"}} onMouseOver={iconMouseOver} onMouseOut={iconMouseOut} onMouseDown={addIntoList} alt="plus"/>
+      </section>
+      {itemsTemplate}
+    </section>
+  );
 }
 
 export default ItemList;
